4a1ea1aab5b6e1615543a4d62b29df5f5014e06d
elizavaidyan <65553237+elizavaidyan@users.noreply.github.com>
2020-12-30 23:39:59 +0530

    Creating Task End Point - Rest API for creating task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,25 +7,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-//Create a Task Model
 
-const Task = new mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-//Create an instance of Task model
-
-const task = new Task ({
-    description: '  Plot the plants  ',
-    //completed: false
-})
 
 
---
+++ src/models/task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+//Create a Task Model
+
+const Task = new mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
