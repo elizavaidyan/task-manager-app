b0192f147a5781c3f2063781d8ea4a6e9338c573
elizavaidyan <65553237+elizavaidyan@users.noreply.github.com>
2020-12-30 20:27:18 +0530

    Mongoose Models- User, Task - Data Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,26 +1,59 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser :true,
     useCreateIndex: true
 })
 
-
 //Create User Model
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim:true,  //To remove space before and after
+        lowercase:true,    //to convert the email to lowercase before saving it.
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type:String,
+        required:true,
+        minLength:7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {  //function, adds a validation function for this property
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 //Create instance of User Model
 const me = new User({
-    name: 'Angel',
-    age: 27
+    name: '   Anu   ',
+     //age: 27
+    //age: -1
+    email: '  anu@GMAIL.COM  ',
+    password: 'anu1234'
 })
 
 //Save instance to the databse
@@ -37,24 +70,28 @@ me.save().then(() => {
 
 const Task = new mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 //Create an instance of Task model
 
-const cleaning = new Task ({
-    description: 'Cleaning the house',
-    completed: false
+const task = new Task ({
+    description: '  Plot the plants  ',
+    //completed: false
 })
 
 //Save the cleaning instance to the databse
 
-cleaning.save().then(() => {
-    console.log(cleaning)
+task.save().then(() => {
+    console.log(task)
 }).catch((error) => {
     console.log('Error!', error)
 })
+
