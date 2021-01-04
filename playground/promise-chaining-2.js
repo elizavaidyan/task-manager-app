5d4d3b953d97d883b052624a778a6a5e42c80f71
elizavaidyan <65553237+elizavaidyan@users.noreply.github.com>
2021-01-05 00:58:33 +0530

    async-await in User and Task Models

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,10 +1,27 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
+/*
 Task.findByIdAndDelete('5fecc011ee88ab52c0a90509').then(() => {
     
     return Task.countDocuments({ completed: false })
 }).then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+})
+
+*/
+
+// Delete Task And Count Using Async-await
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments( { completed: false })
+    return count
+}
+
+deleteTaskAndCount('5fec63a8b19b7a4e880a5d7d').then((count) => {
     console.log(count)
 }).catch((e) => {
     console.log(e)
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,12 +2,26 @@ require('../src/db/mongoose')
 const User = require('../src/models/user')
 
 //Update user by finding by ID
-
+/*
 User.findByIdAndUpdate('5fec909d2605b141a0970e45', { age: 1}).then((user) => {
     console.log(user)
     return User.countDocuments({age: 1})
 }).then((result) => {
     console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
+*/
+//Update age and count (Using async-await)
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5fec909d2605b141a0970e45', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
